239f1d9d3fa70798d89fc27eedb0dac8d017b965
elgohary <ahmedfawyelgohary@yahoo.com>
2023-07-09 16:39:11 +0300

    start in guesse game

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+/*
+document.querySelector('.number').textContent = 14;
+document.querySelector('.score').textContent = 22;
+document.querySelector('.guess').value = 24; 
+*/
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number ❗';
+  }
+});
