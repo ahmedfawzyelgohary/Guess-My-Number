787a541b9351d01de6a0fa5016a69b4bb8481a36
elgohary <ahmedfawyelgohary@yahoo.com>
2023-07-14 20:01:03 +0300

    finish this project

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ document.querySelector('.guess').value = 24;
 */
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,6 +24,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
   }
 
   // when guess to high
